6991799ec83cbce0cb56b27d692334097e399525
Kanisorn Kaewsrithong <kanisornka65@nu.ac.th>
2023-01-10 22:02:30 +0700

    Built on Rock Paper Scissors game to have it sent the players choice to the hive mq broker and tell who win

--- rps_game/app_rps.py
+++ rps_game/app_rps.py
@@ -1,26 +1,29 @@
 # copyright (c) 2022 Kanisorn Kaewsrithong <kanisornka65@nu.ac.th>
 
 """
-Making a GUI to play Rock Paper Scissor game.
-adding 3 buttons to the gui and a label that shows what you have picked
+Rock Paper Scissors game to have it sent the players choice to the hive mq broker
+There will be a server running that will send the computers choice back to you.
+Having GUI display the players choice and who won.
+
 """
 
 ___authore___ = "Kanisorn Kaewsrithong"
 
-import comm_person
+
 import tkinter as tk  # libraly
 
-import random
-
-COM_LIST = ['Rock', 'Scissors', 'Paper']
+import comm_rps
 
 
 class RockPaperScissors:
     """ make class to create GUI """
 
-    def __init__(self, root):
-        self.comm_person = comm_person.MQTTConn(self)
-        self.app = root
+    def __init__(self, master: comm_rps):
+
+        self.comm = comm_rps.MQTTConn(self)
+        self.app = master
+
+
         self.app.title("Rock Paper Scissors IoT game")  # Named to GUI
 
         # Create the buttons for the three options
@@ -47,38 +50,49 @@ class RockPaperScissors:
         self.result_text = tk.Label(self.app, text="", fg="red")
         self.result_text.pack()
 
+        #Variable for keeping player choice
+        self.player_choice = " "
+
     def rock(self):
         """ if we click 'pick rock' button, it will show text as 'You picked Rock' """
         self.player_choice = 'Rock'
         self.person_text.config(text="You picked Rock")
-        self.com()
+        self.TEXT_ON()
+
 
     def scissors(self):
         """ if we click 'pick scissors' button, it will show text as 'You picked Scissors' """
         self.person_text.config(text="You picked Scissors")
         self.player_choice = 'Scissors'
-        self.com()
+        self.TEXT_ON()
+
 
     def paper(self):
         """ if we click 'pick paper' button, it will show text as 'You picked Paper' """
         self.player_choice = 'Paper'
         self.person_text.config(text="You picked Paper")
-        self.com()
+        self.TEXT_ON()
 
-    def com(self):
-        self.com_choice = random.choice(COM_LIST)
-        self.com_text.config(text="Computer picked " + self.com_choice)
+    def TEXT_ON(self):
+        """
+        Publish a choice which we choose in IoT
+        """
         self.msg = self.player_choice
-        self.comm_person.publish(self.msg)
+        self.comm.publish(self.msg)
 
+    def change_status(self, msg_com):
+        """
+        Showing computer choice in GUI and rule of game
+        """
+        self.com_text.config(text="Computer picked "+ msg_com)
 
-        if self.player_choice == self.com_choice:
+        if self.player_choice == msg_com:
             self.result_text.config(text="Draws\n"
                                          "(-_-)")
 
-        elif self.player_choice == 'Rock' and self.com_choice == 'Scissors' \
-                or self.player_choice == 'Scissors' and self.com_choice == 'Paper' \
-                or self.player_choice == 'Paper' and self.com_choice == 'Rock':
+        elif self.player_choice == 'Rock' and msg_com == 'Scissors' \
+                or self.player_choice == 'Scissors' and msg_com == 'Paper' \
+                or self.player_choice == 'Paper' and msg_com == 'Rock':
             self.result_text.config(text="You Win \n"
                                          "(^_^)")
 
@@ -87,7 +101,8 @@ class RockPaperScissors:
                                          "(T_T)")
 
 
-app = tk.Tk()
-app.geometry('400x400')  # size of gui
-RockPaperScissors(app)
-app.mainloop()
+if __name__ == "__main__":
+    app = tk.Tk()
+    app.geometry('400x400')  # size of gui
+    RockPaperScissors(app)
+    app.mainloop()
